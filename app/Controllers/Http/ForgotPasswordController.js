f6dbbdada9318303794a6314fcf2e52988e259e4
Lucas Figueiredo <lukas.liberato14@gmail.com>
2019-08-15 23:27:00 -0300

    Passo 7 - Enviando Email, Configuração do arquivo .env, ForgotPasswordController, forgot_password.edge e resources

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const User = use('App/Models/User') // model user para comparação de email
 const Crypto = require('crypto') // lib para criptografia
+const Mail = use('Mail') // funcionalidade de email do js
 
 class ForgotPasswordController {
   async store ({ request, response }) { // request =  Pega os dados fornecidos; response = A resposta da aplicação
@@ -13,6 +14,17 @@ class ForgotPasswordController {
       user.token_created_at = new Date() // setar a data atual na tabela created_ ...
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'], // caminho da view de email
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` }, // variaveis que estão na view Forgot_password
+        message => { // objeto message
+          message // utilizando o objeto
+            .to(user.email)
+            .from('lucas@mail.com', 'Lucas | Precisava')
+            .subject('Recuperação de Senha')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({ error: 'Algo não deu certo!' })
     }
