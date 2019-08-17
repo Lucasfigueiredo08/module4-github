0e9480f8f039a4be928b412239c4906b4b127c14
Lucas Figueiredo <lukas.liberato14@gmail.com>
2019-08-17 00:32:32 -0300

    Passo 12 - Relacionamento - Configurando Models Project e Task

--- app/Models/User.js
+++ app/Models/User.js
@@ -17,6 +17,14 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  project () {
+    return this.hasMany('App/Models/User')
+  }
+
+  tasks () {
+    return this.hasMany('App/Models/Task')
+  }
 }
 
 module.exports = User
