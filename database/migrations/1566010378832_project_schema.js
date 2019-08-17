0f1e3407c5294987c9459b0420ccc08bcb021e3d
Lucas Figueiredo <lukas.liberato14@gmail.com>
2019-08-17 00:16:01 -0300

    Passo 11 - Criando models de projetos e tarefas

---
+++ database/migrations/1566010378832_project_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProjectSchema extends Schema {
+  up () {
+    this.create('projects', (table) => {
+      table.increments()
+      // tabela de projetos
+      table.integer('user_id') // campo User_id que faz referencia ao id da table users
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table.string('title').notNullable()
+      table.text('description').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('projects')
+  }
+}
+
+module.exports = ProjectSchema
---
+++ database/migrations/1566010391073_tasks_schema.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TasksSchema extends Schema {
+  up () {
+    this.create('tasks', (table) => {
+      table.increments()
+      // tabela de projetos
+      table
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+      // tabela de usuarios
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+        // tabela de arquivos
+      table
+        .integer('file_id')
+        .unsigned()
+        .references('id')
+        .inTable('files')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table.string('title').notNullable()
+      table.text('description')
+      table.timestamp('due_date')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('tasks')
+  }
+}
+
+module.exports = TasksSchema
