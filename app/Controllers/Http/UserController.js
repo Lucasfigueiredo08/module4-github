016d80a30c94356b8a4a2a325e6600db7bada339
Lucas Figueiredo <lukas.liberato14@gmail.com>
2019-08-30 21:09:18 -0300

    Passo 25 - Transactions - Verificação do banco em relação aos comandos, verificação de erro

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Database = use('Database ') // sempre que tiver mais de uma operação no banco de dados, utiliza para garantir que elas vão ser executadas, caso uma falhe, o trx não da o commit
 const User = use('App/Models/User')
 
 class UserController {
@@ -7,9 +8,13 @@ class UserController {
     const data = request.only(['username', 'email', 'password']) // 'confirm_password'
     const addresses = request.input('addresses') // pegar apenas o endereço
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction()
 
-    await user.addresses().createMany(addresses) // relacionamento para cada endereço dentro do array
+    const user = await User.create(data, trx)
+
+    await user.addresses().createMany(addresses, trx) // relacionamento para cada endereço dentro do array
+
+    await trx.commit()
 
     return user
   }
